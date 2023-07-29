ce6991c9c2a06c4fbc616b91664187e1f797a3b9
Daniel Marschall <info@daniel-marschall.de>
2023-07-29 18:27:37 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/uuid_mac_utils/trunk@56 863ceb64-627e-476f-a94c-b2449e5a187f

--- includes/uuid_utils.inc.php
+++ includes/uuid_utils.inc.php
@@ -3,7 +3,7 @@
 /*
  * UUID utils for PHP
  * Copyright 2011 - 2023 Daniel Marschall, ViaThinkSoft
- * Version 2023-07-18
+ * Version 2023-07-29
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
