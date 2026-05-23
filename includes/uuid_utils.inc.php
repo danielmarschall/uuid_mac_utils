b40474e6862b67ab59c8d22e0357a267d66c23be
Daniel Marschall <info@daniel-marschall.de>
2026-05-23 21:21:45 +0000

    Microsoft GUID
    git-svn-id: https://svn.viathinksoft.com/svn/uuid_mac_utils/trunk@105 863ceb64-627e-476f-a94c-b2449e5a187f

--- includes/uuid_utils.inc.php
+++ includes/uuid_utils.inc.php
@@ -898,11 +898,6 @@ function uuid_info($uuid, $echo=true) {
 
 			echo sprintf("%-32s %s\n", "Variant:", "[0b110] Reserved for Microsoft Corporation");
 
-			global $LEGACY_GUID_REGISTRY;
-			if (isset($LEGACY_GUID_REGISTRY[$uuid])) {
-				echo sprintf("%-32s %s\n", "Known GUID:", $LEGACY_GUID_REGISTRY[$uuid]);
-			}
-
 			break;
 		case 3:
 			echo sprintf("%-32s %s\n", "Variant:", "[0b111] Reserved for future use");
@@ -1118,6 +1113,86 @@ function uuid_info($uuid, $echo=true) {
 
 	// END: UUIDv9 (unofficial)
 
+	// START: Microsoft UUIDs
+
+	$guid = sprintf(
+		'%s-%s-%s-%s-%s',
+		substr($uuid, 0, 8),
+		substr($uuid, 8, 4),
+		substr($uuid, 12, 4),
+		substr($uuid, 16, 4),
+		substr($uuid, 20, 12)
+	);
+
+	// ------------------------------------------------------------
+	// OLE GUID
+	// xxxxxxxx-yyyy-zzzz-c000-000000000046
+	// ------------------------------------------------------------
+	if (preg_match('/^([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-c000-000000000046$/i', $guid, $m)) {
+		echo "\n<u>Interpretation of Microsoft GUID</u>\n\n";
+		echo sprintf("%-32s %s\n", "Microsoft Type:", "OLE GUID (xxxxxxxx-yyyy-zzzz-c000-000000000046)");
+		echo sprintf("%-32s DEFINE_OLEGUID(..., 0x%sL, 0x%s, 0x%s)\n", "Win32 API Definition:",
+			strtoupper($m[1]),
+			strtoupper($m[2]),
+			strtoupper($m[3])
+		);
+	}
+
+	// ------------------------------------------------------------
+	// DAO GUID
+	// xxxxxxxx-0000-0010-8000-00aa006d2ea4
+	// ------------------------------------------------------------
+	if (preg_match('/^([0-9a-f]{8})-0000-0010-8000-00aa006d2ea4$/i', $guid, $m)) {
+		echo "\n<u>Interpretation of Microsoft GUID</u>\n\n";
+		echo sprintf("%-32s %s\n", "Microsoft Type:", "DAO GUID (xxxxxxxx-0000-0010-8000-00aa006d2ea4)");
+		echo sprintf("%-32s DEFINE_DAOGUID(..., 0x%sL)\n", "Win32 API Definition:",
+			strtoupper($m[1])
+		);
+	}
+
+	// ------------------------------------------------------------
+	// MediaSubtype
+	// xxxxxxxx-0000-0010-8000-00aa00389b71
+	// ------------------------------------------------------------
+	if (preg_match('/^([0-9a-f]{8})-0000-0010-8000-00aa00389b71$/i', $guid, $m)) {
+		echo "\n<u>Interpretation of Microsoft GUID</u>\n\n";
+		$value = strtoupper($m[1]);
+		$bytes = [
+			hexdec(substr($value, 6, 2)),
+			hexdec(substr($value, 4, 2)),
+			hexdec(substr($value, 2, 2)),
+			hexdec(substr($value, 0, 2)),
+		];
+		$hasZero = in_array(0, $bytes, true);
+		echo sprintf("%-32s %s\n", "Microsoft Type:", "MediaSubtype GUID (xxxxxxxx-0000-0010-8000-00aa00389b71)");
+		if (!$hasZero) {
+			$fourcc =
+				chr($bytes[0]) .
+				chr($bytes[1]) .
+				chr($bytes[2]) .
+				chr($bytes[3]);
+			echo sprintf("%-32s %s\n", "Subtype FourCC:", $fourcc);
+			echo sprintf("%-32s %s\n", "Win32 API Definition:", guidToCppDefine($guid, 'WMMEDIASUBTYPE_'.$fourcc));
+		} else {
+			$magic =
+				  ($bytes[3] << 24)
+				| ($bytes[2] << 16)
+				| ($bytes[1] << 8)
+				|  $bytes[0];
+			echo sprintf("%-32s %u (0x%X)\n", "Subtype Number:", $magic, $magic);
+			echo sprintf("%-32s %s\n", "Win32 API Definition:", guidToCppDefine($guid, 'WMMEDIASUBTYPE_...'));
+		}
+	}
+
+	global $LEGACY_GUID_REGISTRY;
+	if (isset($LEGACY_GUID_REGISTRY[$uuid])) {
+		echo sprintf("%-32s %s\n", "Known GUID:", $LEGACY_GUID_REGISTRY[$uuid]." (<a href=\"https://github.com/danielmarschall/LegacyGuidFinder/tree/main\">Source</a>)");
+	}
+
+	// END: Microsoft UUIDs
+
+	echo "\nLookup this UUID at <a target=\"_blank\" href=\"https://www.magnumdb.com/search?q=$guid\">MagnumDB</a>, <a target=\"_blank\" href=\"https://google.com/search?q=$guid\">Google</a>, <a target=\"_blank\" href=\"https://bing.com/search?q=$guid\">Bing</a>\n";
+
 	if (!$echo) {
 		$out = ob_get_contents();
 		ob_end_clean();
@@ -1130,6 +1205,38 @@ function uuid_info($uuid, $echo=true) {
 function uuid_canonize($uuid) {
 	if (!uuid_valid($uuid)) return false;
 	return oid_to_uuid(uuid_to_oid($uuid));
+}
+
+function guidToCppDefine(string $uuid, string $name = 'GUID_...'): string {
+    // lowercase + {} und - entfernen
+    $clean = strtolower($uuid);
+    $clean = str_replace(['{', '}', '-'], '', $clean);
+
+    if (strlen($clean) !== 32) {
+        return "Invalid GUID";
+    }
+
+    // GUID Teile
+    $data1 = substr($clean, 0, 8);
+    $data2 = substr($clean, 8, 4);
+    $data3 = substr($clean, 12, 4);
+
+    // letzte 8 Bytes
+    $tail = substr($clean, 16);
+
+    $bytes = [];
+    for ($i = 0; $i < strlen($tail); $i += 2) {
+        $bytes[] = '0x' . strtoupper(substr($tail, $i, 2));
+    }
+
+    return sprintf(
+        'DEFINE_GUID(%s, 0x%sL, 0x%s, 0x%s, %s)',
+        $name,
+        strtoupper($data1),
+        strtoupper($data2),
+        strtoupper($data3),
+        implode(', ', $bytes)
+    );
 }
 
 /*
